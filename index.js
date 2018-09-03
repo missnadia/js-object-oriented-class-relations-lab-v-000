24c4e60f826a8f3faece6538856a43893f90b589
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-03 01:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,4 +53,10 @@ class Trip {
       return driver.id === this.driverId
     });
   }
+
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 }
